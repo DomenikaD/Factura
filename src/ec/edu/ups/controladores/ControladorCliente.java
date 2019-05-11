2cec29f53b9a1872efb486933d32c5d61f22e0cc
Domenika Delgado <>
2019-05-10 20:18:55 -0500

    Creacion en el paquete controladores, la clase ControladorCliente

---
+++ src/ec/edu/ups/controladores/ControladorCliente.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controladores;
+
+import ec.edu.ups.modelo.Cliente;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Domenika Delgado
+ */
+public class ControladorCliente {
+    
+    //Atributos + (List)
+    private Set<Cliente> lista;
+    private int codigo;
+
+    
+    public ControladorCliente() {
+        lista = new HashSet<>();
+        codigo = 0;
+    }    
+
+    public int getCodigo() {
+        return ++codigo;
+    }
+        
+    //******C.R.U.D******\\
+    
+    //Create (Crear)
+    public void crear(Cliente objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);        
+    }
+    
+    //Read (Leer)
+    public Cliente read(int codigo){
+        for (Cliente televisor : lista) {
+            if(televisor.getCodigo() == codigo){
+                return televisor;
+            }
+        }        
+        return null;
+    }
+    
+    //Update (Actualizar)
+    public void update(Cliente objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    
+    //Delete (Borrar)
+    public void delete(int codigo){
+        for (Cliente televisor : lista) {
+            if(televisor.getCodigo() == codigo){
+                lista.remove(televisor);
+                break;
+            }
+        }  
+    } 
+    
+    
+}
